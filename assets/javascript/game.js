ccc3be8b3f2d9db86d40cfbde175cb6bffd8523b
AlCast <alvaro.castelan.21@gmail.com>
2018-06-10 11:46:06 -0500

    updated logic to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,69 @@
+var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+//choices
 
-var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var randomLetter = allLetters[Math.floor(Math.random() * allLetters.length)]
-console.log(randomLetter)
-    //choices
+//wins
+var wins = 0;
+//losses
+var loses = 0;
+//guessesLeft
+var guessesLeft = 9;
+//alreadyguessed
+var alreadyGuessed = []
+// generate random letter
+var randomLetter;
+//record key press
 
-    //wins
-    var wins = 0;
-    //losses
-    var loses = 0;
-    //guessesLeft
-    var guessesLeft = 9;
-    //alreadyguessed
-    var alreadyGuessed = []
-    // generate random letter
+function getLetter() {
+    randomLetter = allLetters[Math.floor(Math.random() * allLetters.length)]
+    console.log(randomLetter)
 
-    //record key press
-
-    
-    
-    
-    
-    
-
-    document.onkeyup = function (event) {
-        var letterPressed = event.key.toLowerCase();
-        //check if random letter equals letter guessed 
-        if (letterPressed === randomLetter) {
-            document.getElementById("wins").innerHTML = "Wins: " + (wins+ 1);
-            alreadyGuessed.push(event.key);
-
-            ;
+    something();
+};
 
 
 
-        }
 
-        if ((guessesLeft === 0) && (guessesLeft > 0)) {
-            document.getElementById("losses").innerHTML = "Losses: " + (loses + 1);
-            document.getElementById("left").innerHTML = guessesLeft;
+function something() {
+document.onkeyup = function (event) {
+    var letterPressed = event.key.toLowerCase();
+    //check if random letter equals letter guessed 
+    if (letterPressed === randomLetter) {
+        wins +=1;
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+        alreadyGuessed.push(event.key);
+        getLetter();
 
-        }
-        if (loses === 3) {
-            alert("game over")
+        ;
 
-        }
 
-        
-        else {
 
-            document.getElementById("left").innerHTML = "Guesses left: " + guessesLeft--;
-            alreadyGuessed.push(event.key);
-            document.getElementById("guesses").innerHTML = alreadyGuessed;
-
-        }
     }
-    //trouble/issues
+
+    if (guessesLeft === 0) {
+        loses += 1;
+        document.getElementById("losses").innerHTML = "Losses: " + loses;
+        console.log(loses);
+        document.getElementById("left").innerHTML = guessesLeft;
+        guessesLeft = 9;
+        getLetter();
+
+    }
+    if (loses === 3) {
+        alert("game over")
+        getLetter();
+
+    } else {
+
+        document.getElementById("left").innerHTML = "Guesses left: " + guessesLeft--;
+        alreadyGuessed.push(event.key);
+        document.getElementById("guesses").innerHTML = alreadyGuessed;
+
+    }
+}
+}
+getLetter()
+//trouble/issues
 // Get code random letter to generate
 // get guesses to reset correctly and not go negative
 //reset to run through it again. I think it need to use a function and then call it when need to reset. Couldn't get it work however. 
 //
-
-
